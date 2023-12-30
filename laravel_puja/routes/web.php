a320cd2553f1273426e2bc132f41df91db4c5d71
Fuzaaja <indra22ti@mahasiswa.pcr.ac.id>
2023-12-30 19:23:42 +0700

    PROJEKLARAVELPUJA
    finalProjekMan3medanPuja

---
+++ laravel_puja/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\PegawaiController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layout.index');
+});
+
+// Route::get('/buku', function () {
+//     return view('buku.index');
+// });
+
+
+Route::resource('pegawai', PegawaiController::class);
+
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::resource('siswa', SiswaController::class);
+
+Route::get('/tampilan', [SiswaController::class, 'show'])->name('siswa.show');
+
+
